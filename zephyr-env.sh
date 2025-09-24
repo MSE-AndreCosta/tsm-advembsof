2617465d890c8471f99da0350938fb489751b786
André <andre_miguel_costa@hotmail.com>
2025-09-24 16:04:12 +0200

    chore: update zephyr base path

--- zephyr-env.sh
+++ zephyr-env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export ZEPHYR_BASE=~/dev/hesso/tsm-advembsof/deps/zephyr
+export ZEPHYR_BASE=~/dev/mse/tsm-advembsof/deps/zephyr
 export ZEPHYR_SDK_INSTALL_DIR=~/zephyr-sdk-0.17.2
 export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
